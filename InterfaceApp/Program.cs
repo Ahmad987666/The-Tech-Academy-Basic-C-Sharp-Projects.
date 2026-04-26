a72c6353b6775f3fda29656d6c6d592210553d2a
Ahmad987666 <tamim.maliji@gmail.com>
2026-04-26 15:46:40 -0400

    Polymorphism Assignment

---
+++ InterfaceApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace InterfaceApp
+{
+    //defining an interface called IQuittable
+    public interface IQuittable
+    {
+        //defining a method signature for Quit
+        void Quit();
+    }
+
+    //employee class that implements the IQuittable interface
+    public class Employee : IQuittable
+    {
+        //property for the employee's name
+        public string Name { get; set; }
+        //property for the employee's ID
+        public string Id { get; set; }
+
+        //implementing the Quit() method from the IQuittable interface
+        public void Quit()
+        {
+            //displaying a message that the employee has quit
+            Console.WriteLine($"{Name} with ID {Id} has quit.");
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //creating an instance of the Employee class
+            Employee employee = new Employee
+            {
+                Name = "John Doe",
+                Id = "12345"
+            };
+
+            //using polymorphism to treat the employee as an IQuittable
+            IQuittable quittableEmployee = employee;
+
+            //calling the Quit method to display the quit message
+            quittableEmployee.Quit();
+        }
+    }
+}
